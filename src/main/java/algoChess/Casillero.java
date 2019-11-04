7eb249813cc1ed48c67e43ab06ce9e01bff12ed4
germanlahitte <49814867+germanlahitte@users.noreply.github.com>
2019-11-04 20:43:04 -0300

    Add files via upload

---
+++ src/main/java/algoChess/Casillero.java
@@ -0,0 +1,51 @@
+package algoChess;
+
+public class Casillero { //TODO :falta  --- incompatibilidad posicion-casillero
+
+    private Posicion ubicacion;
+    private Equipo bando;
+    private Disponibilidad estado;
+    private Pieza ocupante;
+
+    public Casillero(Posicion ubicacion,Equipo bando){
+        this.ubicacion = ubicacion;
+        this.bando = bando;
+        this.desocupar();
+    }
+
+    public void desocupar(){
+        this.ocupante = null;
+        this.estado = new Libre();
+    }
+
+    public void ocupar(Pieza aAsignar){
+        this.ocupante = aAsignar;
+        this.estado = new Ocupado();
+    }
+
+    public Posicion siguiente(Direccion enUnaDireccion){
+      return  ubicacion.siguiente(enUnaDireccion);
+    }
+
+    public void agregarPieza(Pieza aAgregar){
+        this.estado.agregarPieza(aAgregar,this);
+    }
+
+    public void quitarPieza(){
+        this.estado.quitarPieza(this);
+    }
+
+    public int distancia(Casillero otroCasillero){
+       return otroCasillero.distanciaA(this.ubicacion);
+    }
+
+    private int distanciaA(Posicion unaPosicion){
+        return unaPosicion.distanciaA(this.ubicacion);
+    }
+
+    //TODO
+    public float calcularDanio(Equipo equipo){
+        float danio=0;
+        return danio;
+    }
+}
---
+++ src/main/java/algoChess/Pieza.java
@@ -0,0 +1,53 @@
+package algoChess;
+
+public class Pieza { //TODO :falta
+
+    private static int costo;
+    private float vida;
+    private Equipo equipo;
+    private Casillero ubicacion;
+    private Arma arma;
+
+    public static int costo(){
+        return Pieza.costo;
+    }
+
+    protected void ocupar(Casillero esteCasillero){
+        this.ubicacion = esteCasillero;
+    }
+
+    protected void desocupar(){
+        this.ubicacion.desocupar();
+    }
+
+    protected int distanciaA(Casillero unCasillero){
+       return unCasillero.distancia(ubicacion);
+    }
+
+    protected void mover(Casillero destino){
+       destino.agregarPieza(this);
+    }
+
+    protected void atacar(Pieza objetivo){
+        objetivo.atacadaDesde(this.ubicacion, this.arma);
+    }
+
+    protected void atacadaDesde(Casillero unCasillero, Arma unArma){
+       quitarVida(unArma.atacar(this, unCasillero.distancia(this.ubicacion)));
+    }
+
+    protected void quitarVida(float danio){
+        this.hacerseDanio(this.ubicacion.calcularDanio(equipo) * danio);
+    }
+
+    protected void hacerseDanio(float danio){
+
+        if(this.vida < danio) {
+            this.vida = 0;
+        } else {
+            this.vida -= danio;
+        };
+    }
+
+
+}
