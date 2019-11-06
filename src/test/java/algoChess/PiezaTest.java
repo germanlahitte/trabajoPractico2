1035a9dd64d0e7a86fbda4eefb171dec02ba434d
camila <camila2007@live.com.ar>
2019-11-06 20:09:46 -0300

    agrego nuevo test de jinete y funciona ok

--- src/test/java/algoChess/PiezaTest.java
+++ src/test/java/algoChess/PiezaTest.java
@@ -66,9 +66,22 @@ class PiezaTest {
         assertEquals(100,jinete.vidaRestante());
     }
 
-   /*@Test
-    public void soldadoPuedeMoverseEnTodasLasDireccionesTest(){
+   @Test
+    public void jineteAtacaADistanciaLejanaYSeLeRestaLaVidaCorrespondienteTest(){
+       Posicion posicion1 = new Posicion(1,1);
+       Posicion posicion2 = new Posicion(7,7);
+       Equipo bando1 = new Rojo();
+       Equipo bando2 = new Azul();
+       Jinete jinete = new Jinete(bando1);
+       Soldado soldado = new Soldado(bando2);
+       Casillero casillero1 = new Casillero(posicion1,bando1);
+       Casillero casillero2 = new Casillero(posicion2,bando2);
+       casillero1.ubicar(jinete);
+       casillero2.ubicar(soldado);
+       jinete.atacar(soldado);
+       assertEquals(85,soldado.vidaRestante());
 
-   }*/
+
+   }
 
 }
