0513230d658f2cedfb67186503bc9de73d47904c
DamianMezaB <damian.mb90@gmail.com>
2019-11-07 23:20:09 -0300

    Agregados los cambios y unificación de branch

--- src/main/java/algoChess/Piezas/Pieza.java
+++ src/main/java/algoChess/Piezas/Pieza.java
@@ -7,7 +7,6 @@ import algoChess.Vida;
 
 public abstract class Pieza { //TODO :falta
 
-   private static int costo;
    private Vida vida;
    protected Equipo equipo;
    protected Casillero casillero;
