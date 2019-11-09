bea9a65840c456696de338482cf5f0c23ba23b01
DamianMezaB <damian.mb90@gmail.com>
2019-11-09 15:22:16 -0300

    mini refactorizacion desocupado->desocupar daño->danio

--- src/test/java/algoChess/UbicacionTest/CasilleroTest.java
+++ src/test/java/algoChess/UbicacionTest/CasilleroTest.java
@@ -255,7 +255,7 @@ class CasilleroTest {
         pieza.asignarCasillero(casillero2);
         pieza2.asignarCasillero(casillero3);
         casillero.agregarPieza(pieza);
-        casillero.desocupado();
+        casillero.desocupar();
         casillero.agregarPieza(pieza2);
 
         assertThrows(CasilleroOcupadoException.class, ()-> casillero.agregarPieza(pieza3));
@@ -263,7 +263,7 @@ class CasilleroTest {
 
     // Test casillero calcula daño a equipo
     @Test
-    public void testCasilleroDevuelveMultiplicadoDeDañoPorCincoAEquipoDiferente(){
+    public void testCasilleroDevuelveMultiplicadoDeDanioPorCincoAEquipoDiferente(){
         Tablero tablero = new Tablero();
         Posicion posicionRojo = new Posicion(3,3);
         Casillero casillero = tablero.casilleroEn(posicionRojo);
@@ -273,7 +273,7 @@ class CasilleroTest {
     }
 
     @Test
-    public void testCasilleroDevuelveMultiplicadoDeDañoPorUnoAMismoEquipo(){
+    public void testCasilleroDevuelveMultiplicadoDeDanioPorUnoAMismoEquipo(){
         Tablero tablero = new Tablero();
         Posicion posicionRojo = new Posicion(3,3);
         Casillero casillero = tablero.casilleroEn(posicionRojo);
