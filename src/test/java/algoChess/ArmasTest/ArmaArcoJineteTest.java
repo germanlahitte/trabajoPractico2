b9fe058c85e3d31b423a93e7b859809c26a9f0d7
Martin <mdabat@baufest.com>
2019-11-11 17:45:35 -0300

    Actualizo test de ArmaArcoJinete y ArmaCatapulta

--- src/test/java/algoChess/ArmasTest/ArmaArcoJineteTest.java
+++ src/test/java/algoChess/ArmasTest/ArmaArcoJineteTest.java
@@ -7,64 +7,61 @@ import algoChess.Piezas.Soldado;
 import algoChess.Ubicacion.Casillero;
 import algoChess.Ubicacion.Posicion;
 import algoChess.Ubicacion.Tablero;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ArmaArcoJineteTest {
 
 
-    @Test
-    public void testAtacarAPiezaADistanciaMayorNoDisminuyeVida(){
+    private Pieza soldado;
+    private Arma arma;
+
+    @BeforeEach
+    private void executedBeforeEach() {
+
         Tablero tablero = new Tablero();
         Posicion posicionRojo = new Posicion(1,1); // Posicion de equipo rojo.
         Casillero casillero = tablero.casilleroEn(posicionRojo);
 
-        Pieza soldado = new Soldado(new EquipoRojo());
+        soldado = new Soldado(new EquipoRojo());
         soldado.asignarCasillero(casillero);
+    }
 
-        Arma arma = new ArmaArcoJinete();
+    @Test
+    public void testAtacarAPiezaADistanciaMayorNoDisminuyeVida(){
+        arma = new ArmaArcoJinete();
         arma.atacarA(soldado,6);
+
         assertEquals(100,soldado.vida());
+
     }
 
     @Test
     public void testAtacarAPiezaADistanciaMenorNoDisminuyeVida(){
-        Tablero tablero = new Tablero();
-        Posicion posicionRojo = new Posicion(1,1); // Posicion de equipo rojo.
-        Casillero casillero = tablero.casilleroEn(posicionRojo);
-
-        Pieza soldado = new Soldado(new EquipoRojo());
-        soldado.asignarCasillero(casillero);
-
-        Arma arma = new ArmaArcoJinete();
+        arma = new ArmaArcoJinete();
         arma.atacarA(soldado,1);
+
         assertEquals(100,soldado.vida());
     }
 
+
+
     @Test
     public void testAtacarAPiezaADistanciaCorrectaDisminuyeVida(){
-        Tablero tablero = new Tablero();
-        Posicion posicionRojo = new Posicion(1,1); // Posicion de equipo rojo.
-        Casillero casillero = tablero.casilleroEn(posicionRojo);
-
-        Pieza soldado = new Soldado(new EquipoRojo());
-        soldado.asignarCasillero(casillero);
-
-        Arma arma = new ArmaArcoJinete();
+        arma = new ArmaArcoJinete();
         arma.atacarA(soldado,3);
+
         assertEquals(85,soldado.vida());
     }
 
     @Test
     public void testAtacarAPiezaADistanciaCorrectaNoDisminuyeVidaMenorACero(){
-        Tablero tablero = new Tablero();
-        Posicion posicionRojo = new Posicion(1,1); // Posicion de equipo rojo.
-        Casillero casillero = tablero.casilleroEn(posicionRojo);
-
-        Pieza soldado = new Soldado(new EquipoRojo());
-        soldado.asignarCasillero(casillero);
-
-        Arma arma = new ArmaArcoJinete();
+        arma = new ArmaArcoJinete();
         arma.atacarA(soldado,3);
         arma.atacarA(soldado,3);
         arma.atacarA(soldado,3);
--- src/test/java/algoChess/ArmasTest/ArmaCatapultaTest.java
+++ src/test/java/algoChess/ArmasTest/ArmaCatapultaTest.java
@@ -8,49 +8,45 @@ import algoChess.Piezas.Soldado;
 import algoChess.Ubicacion.Casillero;
 import algoChess.Ubicacion.Posicion;
 import algoChess.Ubicacion.Tablero;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ArmaCatapultaTest {
 
-    // Atacar Piezas
-    @Test
-    public void testAtacarAPiezaADistanciaMenorNoDisminuyeVida(){
+
+    private Pieza soldado;
+    private Arma arma;
+    @BeforeEach
+    public void executedBeforeEach() {
         Tablero tablero = new Tablero();
         Posicion posicionRojo = new Posicion(1,1); // Posicion de equipo rojo.
         Casillero casillero = tablero.casilleroEn(posicionRojo);
 
-        Pieza soldado = new Soldado(new EquipoRojo());
+        soldado = new Soldado(new EquipoRojo());
         soldado.asignarCasillero(casillero);
+    }
 
+
+    // Atacar Piezas
+    @Test
+    public void testAtacarAPiezaADistanciaMenorNoDisminuyeVida(){
         Arma armaCatapulta = new ArmaCatapulta();
         armaCatapulta.atacarA(soldado,1);
+
         assertEquals(100,soldado.vida());
     }
 
     @Test
     public void testAtacarAPiezaADistanciaMayorDisminuyeVida(){
-        Tablero tablero = new Tablero();
-        Posicion posicionRojo = new Posicion(1,1); // Posicion de equipo rojo.
-        Casillero casillero = tablero.casilleroEn(posicionRojo);
-
-        Pieza soldado = new Soldado(new EquipoRojo());
-        soldado.asignarCasillero(casillero);
-
         Arma armaCatapulta = new ArmaCatapulta();
         armaCatapulta.atacarA(soldado,6);
+
         assertEquals(80,soldado.vida());
     }
 
     @Test
     public void testAtacarAPiezaADistanciaCorrectaNoDisminuyeVidaMenorACero(){
-        Tablero tablero = new Tablero();
-        Posicion posicionRojo = new Posicion(1,1); // Posicion de equipo rojo.
-        Casillero casillero = tablero.casilleroEn(posicionRojo);
-
-        Pieza soldado = new Soldado(new EquipoRojo());
-        soldado.asignarCasillero(casillero);
-
         Arma armaCatapulta = new ArmaCatapulta();
         armaCatapulta.atacarA(soldado,6);
         armaCatapulta.atacarA(soldado,6);
@@ -60,6 +56,7 @@ class ArmaCatapultaTest {
         armaCatapulta.atacarA(soldado,6);
         armaCatapulta.atacarA(soldado,6);
         armaCatapulta.atacarA(soldado,6);
+
         assertEquals(0,soldado.vida());
     }
 }
