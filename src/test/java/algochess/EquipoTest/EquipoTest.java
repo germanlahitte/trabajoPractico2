b0124643141ab655e7a6c081550ff1661ec8b70f
Germán <germanlahitte@gmail.com>
2019-11-19 19:32:00 -0300

    EquipoTest: pruebo nuevos métodos

--- src/test/java/algochess/EquipoTest/EquipoTest.java
+++ src/test/java/algochess/EquipoTest/EquipoTest.java
@@ -59,4 +59,23 @@ class EquipoTest {
         assertThrows(CasilleroEnemigoException.class, ()-> rojo.ubicarCon(azul, pieza));
     }
 
+    @Test
+    void testEquipoRojoNoEsAliadoDeEquipoAzul(){
+        assertEquals(false,rojo.soyAliado(azul));
+    }
+
+    @Test
+    void testEquipoAzulNoEsAliadoDeEquipoRojo(){
+        assertEquals(false,azul.soyAliado(rojo));
+    }
+
+    @Test
+    void testEquipoAzulEsAliadoDeEquipoAzul(){
+        assertEquals(true,azul.soyAliado(azul));
+    }
+
+    @Test
+    void testEquipoRojoEsAliadoDeEquipoRojo(){
+        assertEquals(true,rojo.soyAliado(rojo));
+    }
 }
