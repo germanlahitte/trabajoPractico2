e6130f1f99739accfb3e9bcd6f56df06f3f49942
camila <camila2007@live.com.ar>
2019-11-24 21:06:13 -0300

    Se muestra pantalla inicial con boton de comenzar juego, muy pobre todo

--- src/main/java/algochess/Main.java
+++ src/main/java/algochess/Main.java
@@ -2,25 +2,23 @@ package algochess;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
-public class Main extends Application {
-    private int numero=0;
-    public int getNumero() {
-        return numero;
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(new Pane(),800,600));
-        stage.show();
-    }
-
-
-}
+//
+//public class Main extends Application {
+//
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//
+//    @Override
+//    public void start(Stage stage) throws Exception {
+//        Image image = new Image("fondo2.jpg");
+//        stage.setScene(new Scene(new Pane(),800,600));
+//        stage.show();
+//    }
+//
+//
+//}
--- src/test/java/algochess/PiezasTest/JineteTest.java
+++ src/test/java/algochess/PiezasTest/JineteTest.java
@@ -10,7 +10,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class JineteTest {
+class
+JineteTest {
 
     @Test
     void testJineteSeCreaConVidaCorrecta() {
