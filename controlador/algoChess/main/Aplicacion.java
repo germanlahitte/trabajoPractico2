340245fae3bb882b119867bf307db5153e223717
camila <camila2007@live.com.ar>
2019-11-25 18:04:48 -0300

    boton comenzar te manda a otra escena

--- controlador/algoChess/main/Aplicacion.java
+++ controlador/algoChess/main/Aplicacion.java
@@ -1,17 +1,20 @@
 package main;
 
+import contenedores.MenuPrincipal;
 import contenedores.PantallaInicial;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 
 public class Aplicacion extends Application {
     @Override
 
     public void start(Stage stage) throws Exception {
         stage.setTitle("AlgoChess");
-
-        PantallaInicial pantallaInicial = new PantallaInicial(stage);
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        Scene escenaDeMenu = new Scene(menuPrincipal,2880,1800);
+        PantallaInicial pantallaInicial = new PantallaInicial(stage,escenaDeMenu);
         Scene escenaDeComienzoDelJuego = new Scene(pantallaInicial,2880,1800);
         stage.setScene(escenaDeComienzoDelJuego);
         stage.setFullScreen(true);
---
+++ vista/javaFX/contenedores/MenuPrincipal.java
@@ -0,0 +1,8 @@
+package contenedores;
+
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MenuPrincipal extends VBox {
+
+}
