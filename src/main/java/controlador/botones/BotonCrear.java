5480af1fdfc178bcc7406ec3f84d8a44d22c07e0
germanlahitte <49814867+germanlahitte@users.noreply.github.com>
2019-11-27 09:55:57 -0300

    Update BotonCrear.java

--- src/main/java/controlador/botones/BotonCrear.java
+++ src/main/java/controlador/botones/BotonCrear.java
@@ -6,6 +6,7 @@ import javafx.event.EventHandler;
 import javafx.scene.control.TextField;
 import modelo.Jugador;
 import modelo.equipos.Equipo;
+import modelo.ubicacion.Tablero;
 
 import java.util.ArrayList;
 
@@ -14,17 +15,19 @@ public class BotonCrear implements EventHandler<ActionEvent> {
     Equipo bando;
     String nombre;
     ArrayList<Jugador> lista;
-
-    public BotonCrear(TextField nombre, Equipo bando, ArrayList<Jugador> lista){
+    Tablero tablero;
+    
+    public BotonCrear(TextField nombre, Equipo bando, Juego batalla){
         this.bando = bando;
         this.nombre = nombre.getText();
-        this.lista = lista;
+        this.lista = batalla.getRonda();
+        this.tablero = batalla.getTablero();
     }
 
 
     @Override
     public void handle(ActionEvent actionEvent) {
-        Jugador jugador = new Jugador(nombre,bando);
+        Jugador jugador = new Jugador(nombre,bando,tablero);
         lista.add(jugador);
     }
 
