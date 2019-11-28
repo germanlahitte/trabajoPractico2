f2f25ab74406cbf85bab536c97c430f2bd5ad424
DamianMezaB <damian.mb90@gmail.com>
2019-11-28 13:36:07 -0300

    renderización de tablero

--- src/main/java/vista/contenedores/TableroView.java
+++ src/main/java/vista/contenedores/TableroView.java
@@ -7,13 +7,14 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import modelo.Jugador;
+import modelo.ProveedorConstantes;
 import modelo.ubicacion.Tablero;
 
 public class TableroView extends Group {
     public double width;
     public double heigth;
-    private double tileWidth = 64;
-    private double tileHeigth = 64;
+    private double tileWidth = 32;
+    private double tileHeigth = 32;
 
     private GridPane table;
 
@@ -21,16 +22,16 @@ public class TableroView extends Group {
 
     public TableroView(Tablero map){
         table = new GridPane();
-        width = tileWidth * 20;
-        heigth = tileHeigth * 20;
+        width = tileWidth * ProveedorConstantes.ladoDelTablero();
+        heigth = tileHeigth * ProveedorConstantes.ladoDelTablero();
         panes = new Pane[ (int)width][(int)heigth];
         Background bi = new Background(new BackgroundImage(new Image("file:src/main/java/vista/imagenes/casillero.png"),
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundPosition.CENTER,
                 new BackgroundSize(tileWidth, tileHeigth, false, false, false, false)));
-            for (int i = 0; i < 20; i++) {
-                for (int j = 0; j < 20; j++) {
+            for (int i = 0; i < ProveedorConstantes.ladoDelTablero(); i++) {
+                for (int j = 0; j < ProveedorConstantes.ladoDelTablero(); j++) {
                     Pane v = new Pane();
                     v.setMinHeight(this.tileHeigth);
                     v.setMinWidth(this.tileWidth);
