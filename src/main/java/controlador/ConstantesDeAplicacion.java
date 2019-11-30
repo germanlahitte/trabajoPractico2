4edbd5d51519c33158b6c847fcccec5f25aa618b
DamianMezaB <damian.mb90@gmail.com>
2019-11-30 15:51:25 -0300

    tableroview ya no usa constantes para el lado ladodeltablero se quito de ConstantesDeAplicacion

--- src/main/java/controlador/ConstantesDeAplicacion.java
+++ src/main/java/controlador/ConstantesDeAplicacion.java
@@ -5,12 +5,8 @@ public class ConstantesDeAplicacion {
     private static int ANCHO_VENTANA = 800;
     private static int ALTO_VENTANA = 600;
 
-    private static int LADO_DEL_TABLERO = 20;
-
     public static int getAnchoVentana() { return ANCHO_VENTANA; }
     public static int getAltoVentana() { return ALTO_VENTANA; }
-
-    public static int ladoDelTablero() { return LADO_DEL_TABLERO; }
 
 }
 
